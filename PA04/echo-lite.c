130dd53846a21e949a176c753b2a1414da1c6be3
Nicolas Mauricio Herrera <herreran@ecegrid-thin1.ecn.purdue.edu>
2014-09-27 23:37:23 -0400

    commit echo-lite.c

--- PA04/echo-lite.c
+++ PA04/echo-lite.c
@@ -1,8 +1,22 @@
 #include <stdio.h>
 #include <stdlib.h>
+#include <string.h>
 
 int main(int argc, char ** argv)
 {
+    int i;
+    for(i = 1; i < argc; i++)
+    {
+	//if(!strstr(argv[i], "\""))
+	//{
+	    printf("%s ", argv[i]);
+	/*}
+	else
+	{
+
+	}*/
+    }    
+    printf("\n");
 
     return EXIT_SUCCESS;
 }
