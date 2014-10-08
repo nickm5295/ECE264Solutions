c640388314de2fb9100f1cb4d0ce9d0b00f98af7
Nicolas Mauricio Herrera <herreran@ecegrid-thin1.ecn.purdue.edu>
2014-10-08 17:01:28 -0400

    commit answer05.c

--- PA05/answer05.c
+++ PA05/answer05.c
@@ -9,7 +9,9 @@ void printArray(int *arr, int len);
 void initiateArray(int *result, int initialNum, int size);
 void spread(int *loc, int remainder, int prev, int *length);
 void partInc(int *result, int size);
-
+void printArrayBack(int *arr, int len);
+void printResultBack(int *arr, int len);
+void partOdd(int *loc, int rem);
 
 void partitionAll(int value)
 {
@@ -18,6 +20,71 @@ void partitionAll(int value)
     printResult(result, value);
     partition(result, value);
     free(result);
+}
+
+void partitionOdd(int value)
+{
+    if(value % 2 != 1)
+    {
+	return;
+    }
+    int *result = malloc(sizeof(int) * value);
+    //int length = 0;
+    initiateArray(result, 1, value);
+    printResult(result, value);
+    partOdd(result, value);
+    free(result);
+}
+
+void partOdd(int *result, int value)
+{
+    int len = value - 1;
+    int i;
+    while(len > 0)
+    {
+	if(result[len] == 1)
+  	{
+	    (result[len - 2])+= 2;
+	    (result[len])--;
+	    (result[len - 1])--;
+	    for(i = result[len - 1]; i > 0; i--)
+            {
+                result[len - 1] = 1;
+                len++;
+            }
+	    len -= 2;
+	}
+	else
+	{
+	    (result[len-1])+=2;
+	    (result[len]) -= 2;
+            for(i = result[len]; i > 0; i--)
+	    {
+		result[len] = 1;
+	    	len++;
+	    }
+	    len--;
+	}
+	printResult(result, len + 1);
+    }
+    /*int i;
+    int oRem = rem;
+    (*length)++;
+    for(i = 1; i <= oRem; i+=2)
+    {
+	*loc = i;
+	rem--;
+	if(rem > 0)
+	{
+	    partOdd(loc+1, rem, length);
+	}
+    }
+    for(i = 1; i <= *length - 1; i++)
+    {
+	printf("%d, ", loc[i - *length]);
+    }
+    printf("%d\n", loc[i - *length]);
+    (*length)--;*/
 }
 
 void partition(int *result, int length)
@@ -53,6 +120,26 @@ void printResult(int *arr, int len)
     printArray(arr, len);
 }
 
+void printResultBack(int *arr, int len)
+{
+    printf("= ");
+    printArrayBack(arr, len);
+}
+
+void printArrayBack(int *arr, int len)
+{
+    int i;
+    if(len > 0)
+    {
+	for(i = len - 1; i > 0; i--)
+	{
+	    printf("%d, ", arr[i]);
+	}
+	printf("%d", arr[i]);
+    }
+    printf("\n");
+}
+
 void printArray(int *arr, int len)
 {
     int i;
@@ -80,6 +167,31 @@ void partitionIncreasing(int value)
 {
     int *result = malloc(sizeof(int) * (value / 2 + 2));
     partInc(result, value);
+    free(result);
+}
+
+void partitionDecreasing(int size)
+{
+    int remainder, prev;
+    int length = 0;
+    int *result = malloc(sizeof(int) * (size / 2 + 2));
+    spread(result, size, 0, &length);
+    printResultBack(result, length);
+    while(length != 1)
+    {
+	length--;
+	prev = ++(result[length - 1]);
+        remainder = result[length] - 1;
+        if(remainder > prev)
+        {
+            spread(result+length, remainder, prev, &length);
+        }
+        else
+        {
+            (result[length - 1]) += remainder;
+        }
+        printResultBack(result, length);
+    }
     free(result);
 }
 
